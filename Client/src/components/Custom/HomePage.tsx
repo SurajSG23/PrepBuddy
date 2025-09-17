109245bc38e4b857268a8aa17e63917b5e28b6ee
Suraj S G Dhanva <164147518+SurajSG23@users.noreply.github.com>
2025-09-17 23:27:42 +0530

    Update HomePage.tsx

--- Client/src/components/Custom/HomePage.tsx
+++ Client/src/components/Custom/HomePage.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { cn } from "../../../lib/utils";
 import { useDarkMode } from "../Custom/DarkModeContext";
 
 interface HeaderProps {
